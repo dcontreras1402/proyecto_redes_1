d60e9649b543ccbc29f52881b48a992edfff6076
dcontreras1 <daniel.contreras_d@uao.edu.co>
2026-04-21 20:30:51 -0500

    Update config.js & pages/

--- frontend/buyza-frontend/js/config.js
+++ frontend/buyza-frontend/js/config.js
@@ -3,25 +3,15 @@ const CONFIG = {
 
   PORTS: {
     USUARIOS: 3001,
-    CREDITOS: 3002,
-    CATALOGO: 3003,
-    ORDENES:  3004,
-    PAGOS:    3005,
+    CATALOGO: 3002,
+    ORDENES:  3003,
+    PAGOS:    3004,
+    CREDITO:  3005,
   },
 
-  get USUARIOS_URL() {
-    return `${this.API_BASE_URL}:${this.PORTS.USUARIOS}/api/usuarios/`;
-  },
-  get CREDITOS_URL() {
-    return `${this.API_BASE_URL}:${this.PORTS.CREDITOS}/api/credito/`;
-  },
-  get CATALOGO_URL() {
-    return `${this.API_BASE_URL}:${this.PORTS.CATALOGO}/api/catalogo/`;
-  },
-  get ORDENES_URL() {
-    return `${this.API_BASE_URL}:${this.PORTS.ORDENES}/api/ordenes/`;
-  },
-  get PAGOS_URL() {
-    return `${this.API_BASE_URL}:${this.PORTS.PAGOS}/api/pagos/`;
-  },
+  get USUARIOS_URL() { return `${this.API_BASE_URL}:${this.PORTS.USUARIOS}/api/usuarios`; },
+  get CATALOGO_URL() { return `${this.API_BASE_URL}:${this.PORTS.CATALOGO}/api/catalogo`; },
+  get ORDENES_URL()  { return `${this.API_BASE_URL}:${this.PORTS.ORDENES}/api/ordenes`; },
+  get PAGOS_URL()    { return `${this.API_BASE_URL}:${this.PORTS.PAGOS}/api/pagos`; },
+  get CREDITO_URL()  { return `${this.API_BASE_URL}:${this.PORTS.CREDITO}/api/credito`; },
 };
